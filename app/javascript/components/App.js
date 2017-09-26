157af87cdb1504b00ee6736f47bb5bc1c135052c
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-09-26 11:11:47 -0400

    Required application.js inside application.html layout file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Test from './Test'
 
 class App extends Component {
   render () {
     return <div className='App'>
       Hello
+      <Test />
     </div>
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,8 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
+
+import Test from '../components/Test'
+
+console.log(Test)
